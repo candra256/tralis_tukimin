98ca65f195d56dc3793202054d49fe8839ecaefc
candra <dalangpelo982@gmail.com>
2024-03-14 21:59:47 -1200

    modif

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 document.addEventListener("alpine:init", ()=> {
      Alpine.data("products", ()=> ({
           items: [
-               {id:1,img:'1.jpg'},
-               {id:2, img:'2.jpg'},
-               {id:3,img:'2.jpg'},
-               {id:4, img:'1.jpg'},
-               {id:5,img:'1.jpg'},
-               {id:6, img:'2.jpg'},
+               {id:1,img:'1.jpg', nama:"motif1"},
+               {id:2, img:'2.jpg',nama:"motif2"},
+               {id:3,img:'2.jpg',nama:"motif3"},
+               {id:4, img:'1.jpg',nama:"motif4"},
+               {id:5,img:'1.jpg',nama:"motif5"},
+               {id:6, img:'2.jpg', nama:"motif6"},
           ]
      }));
 
